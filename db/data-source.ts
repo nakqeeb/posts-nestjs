a77ef6a8b6bc8ba643e18c3eae7233e2ab8ae165
Yafey <kjbn.me@gmail.com>
2022-12-15 10:37:44 +0300

    updaate variables

--- db/data-source.ts
+++ db/data-source.ts
@@ -26,7 +26,11 @@ switch (process.env.NODE_ENV) {
   case 'production':
     Object.assign(dataSourceOptions, {
       type: 'postgres',
-      url: process.env.DATABASE_URL,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       migrationsRun: true,
       entities: ['dist/**/*.entity.js'],
       ssl: {
