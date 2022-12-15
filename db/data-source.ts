f3c965b0efcf1053e6d8c9241630da57646822e2
Yafey <kjbn.me@gmail.com>
2022-12-15 21:46:45 +0300

    updated

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,56 +1,19 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-const dotenv = require('dotenv');
-const parsed = dotenv.config({ path: '.env.production' });
+/* const dotenv = require('dotenv');
+const parsed = dotenv.config({ path: '.env.production' }); */
 export const dataSourceOptions: DataSourceOptions = {
-  //   type: 'sqlite',
-  //   database: 'postsdb.sqlite',
-  //   entities: ['dist/**/*.entity.js'],
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  migrationsRun: true,
+  entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js'],
-  // synchronize: true,
-} as DataSourceOptions;
-
-switch (process.env.NODE_ENV) {
-  case 'development':
-    Object.assign(dataSourceOptions, {
-      type: 'sqlite',
-      database: 'postsdb.sqlite',
-      entities: ['dist/**/*.entity.js'],
-    });
-    break;
-  case 'test':
-    Object.assign(dataSourceOptions, {
-      type: 'sqlite',
-      database: 'test.sqlite',
-      entities: ['dist/**/*.entity.ts'],
-    });
-    break;
-  case 'production':
-    Object.assign(dataSourceOptions, {
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      migrationsRun: true,
-      entities: ['dist/**/*.entity.js'],
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    });
-    break;
-  default:
-    Object.assign(dataSourceOptions, {
-      type: 'postgres',
-      url: parsed.parsed.DATABASE_URL,
-      migrationsRun: true,
-      entities: ['**/*.entity.js'],
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    });
-  // throw new Error('unknown environment');
-}
-
-//console.log(process.env.DATABASE_URL);
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  synchronize: false,
+};
 
 const dataSource = new DataSource(dataSourceOptions);
 export default dataSource;
