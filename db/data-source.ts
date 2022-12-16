32234f3cd3bf3501974b85dc99bd714ff8df643f
Yafey <kjbn.me@gmail.com>
2022-12-16 07:43:24 +0300

    fixed

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,20 +1,57 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 /* const dotenv = require('dotenv');
 const parsed = dotenv.config({ path: '.env.production' }); */
+// console.log(parsed);
 export const dataSourceOptions: DataSourceOptions = {
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
-  username: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  migrationsRun: true,
-  entities: ['dist/**/*.entity.js'],
+  //   type: 'sqlite',
+  //   database: 'postsdb.sqlite',
+  //   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js'],
-  //synchronize: false,
-};
+  synchronize: false,
+} as DataSourceOptions;
+
+console.log(`This is it ${process.env.NODE_ENV}`);
+switch (process.env.NODE_ENV) {
+  case 'development':
+    Object.assign(dataSourceOptions, {
+      type: 'sqlite',
+      database: 'postsdb.sqlite',
+      entities: ['dist/**/*.entity.js'],
+    });
+    break;
+  case 'test':
+    Object.assign(dataSourceOptions, {
+      type: 'sqlite',
+      database: 'test.sqlite',
+      entities: ['dist/**/*.entity.ts'],
+    });
+    break;
+  case 'production':
+    Object.assign(dataSourceOptions, {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      migrationsRun: true,
+      entities: ['dist/**/*.entity.js'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+    break;
+  default:
+    // Object.assign(dataSourceOptions, {
+    //   type: 'postgres',
+    //   url: parsed.parsed.DATABASE_URL,
+    //   migrationsRun: true,
+    //   entities: ['**/*.entity.js'],
+    //   ssl: {
+    //     rejectUnauthorized: false,
+    //   },
+    // });
+    throw new Error('unknown environment');
+}
+
+//console.log(process.env.DATABASE_URL);
 
 const dataSource = new DataSource(dataSourceOptions);
 export default dataSource;
